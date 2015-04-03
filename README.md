e2a969c09714ca181022d973e40228b85dbfce82
Zeljko Simic <zeljkosbgd@gmail.com>
2015-04-03 15:33:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+compressor-g4
+======
+
+> compressor-g4 - [nodejs](http://nodejs.org) library
+
+## Install
+
+    $ npm install compressor-g4
+
+## Usage
+
+## Development
+
+### Install dependencies
+
+    $ make install
+
+### Run tests
+
+    $ make test
+
+## License
+
+(The MIT License)
+see LICENSE file for details...
---
+++ test/lib/compressor-g4.test.js
@@ -0,0 +1,19 @@
+
+var should = require("chai").should(),
+    suite = require("../suite"),
+    compressor-g4 = require(suite.path("compressor-g4"));
+
+describe("compressor-g4", function(){
+
+    beforeEach(function() {
+
+    });
+
+    describe("", function() {
+
+        it("", function() {
+
+        });
+    });
+
+});
