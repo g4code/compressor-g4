a83e494584c9eeec219a80ff48abee30c4fb690f
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-10-04 15:16:09 +0200

    Moved path parsing and reading options from conf in new options object

--- bin/compressor.js
+++ bin/compressor.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 
-var compressor = require("../lib/compressor"),
-    commander      = require("commander"),
-    packageData = require("../package.json");
+const App           = require("../lib/app")
+const Options       = require('../lib/options')
+const commander     = require("commander")
+const packageData   = require("../package.json")
+const evento        = require("evento")
+const informer      = require("informer")
+
+informer.title(packageData.name)
+    .titleColor("grey");
+
+evento.on("INFORMER|ERROR",     informer.error.bind(informer));
+evento.on("INFORMER|INFO",      informer.info.bind(informer));
+evento.on("INFORMER|LOADING",   informer.loading.bind(informer));
+evento.on("INFORMER|SUCCESS",   informer.success.bind(informer));
+evento.on("INFORMER|WARNING",   informer.warning.bind(informer));
+
+evento.on("COMMANDER|HELP",     function() {
+    commander.outputHelp()
+})
 
 commander.version(packageData.version)
     .usage("[options] [dir]")
@@ -10,4 +26,4 @@ commander.version(packageData.version)
     .option('-e, --env <n>', 'environment (e.g. production, stage, dev, local ...)')
     .parse(process.argv);
 
-compressor.run(process.cwd()+'/'+commander.config, commander.env);
+new App(new Options(commander.config, commander.env)).run()
