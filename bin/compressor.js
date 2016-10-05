cc406c571363ebcdff865902dad443932c3808c2
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-10-05 11:41:12 +0200

    Messages object; info for all options

--- bin/compressor.js
+++ bin/compressor.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
-const App           = require("../lib/app")
-const Options       = require('../lib/options')
 const commander     = require("commander")
-const packageData   = require("../package.json")
 const evento        = require("evento")
 const informer      = require("informer")
+const packageData   = require("../package.json")
+const Compressor    = require("../lib/compressor")
+const Options       = require('../lib/options')
+const messages      = require('../lib/messages')
 
 informer.title(packageData.name)
     .titleColor("grey");
 
-evento.on("INFORMER|ERROR",     informer.error.bind(informer));
-evento.on("INFORMER|INFO",      informer.info.bind(informer));
-evento.on("INFORMER|LOADING",   informer.loading.bind(informer));
-evento.on("INFORMER|SUCCESS",   informer.success.bind(informer));
-evento.on("INFORMER|WARNING",   informer.warning.bind(informer));
+evento.on(messages.informerError,   informer.error.bind(informer));
+evento.on(messages.informerInfo,    informer.info.bind(informer));
+evento.on(messages.informerLoading, informer.loading.bind(informer));
+evento.on(messages.informerSuccess, informer.success.bind(informer));
+evento.on(messages.informerWarning, informer.warning.bind(informer));
 
-evento.on("COMMANDER|HELP",     function() {
+evento.on(messages.commanderHelp, function() {
     commander.outputHelp()
 })
 
@@ -26,4 +27,4 @@ commander.version(packageData.version)
     .option('-e, --env <n>', 'environment (e.g. production, stage, dev, local ...)')
     .parse(process.argv);
 
-new App(new Options(commander.config, commander.env)).run()
+new Compressor(new Options(commander.config, commander.env)).run()
